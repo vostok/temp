877e652a6963c24beec3594755c9691e205bf3bd
kungurtsev <kungurtsev@skbkontur.ru>
2019-08-15 18:38:52 +0500

    some tests

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# temp
+# Temp
+
+[![Build status](https://ci.appveyor.com/api/projects/status/github/vostok/?svg=true&branch=master)](https://ci.appveyor.com/project/vostok//branch/master)
+[![NuGet](https://img.shields.io/nuget/v/Temp.svg)](https://www.nuget.org/packages/Temp)
+
+
+
+
+**Build guide**: https://github.com/vostok/devtools/blob/master/library-dev-conventions/how-to-build-a-library.md
+
+**User documentation**: not written yet.
---
+++ Temp.Tests/Tests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using Vostok.Logging.Abstractions;
+using Vostok.Logging.Console;
+
+namespace Temp.Tests
+{
+    [TestFixture]
+    internal class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            new SynchronousConsoleLog().Info("Test1");
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var log = new SynchronousConsoleLog();
+            for (int i = 0; i < 10; i++)
+                log.Info("Test2 {i}", i);
+        }
+    }
+}
